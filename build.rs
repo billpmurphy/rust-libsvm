e8ce85ae2d4c091619ac0ce41d54b5188548749b
Rhonin <yc_rhonin@qq.com>
2021-09-09 10:19:48 +0800

    try! -> ?; dependencies gcc->cc; fix UB; fmt project;

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
-extern crate gcc;
-
 fn main() {
-    println!("cargo:rustc-flags=-l dylib=stdc++");
-    gcc::compile_library("libsvm.a", &["libsvm/svm.cpp"]);
+    cc::Build::new()
+        .file("libsvm/svm.cpp")
+        .flag("-std=c++11")
+        .compile("libsvm.a");
 }
--- src/sys.rs
+++ src/sys.rs
@@ -4,13 +4,13 @@
 pub struct CSVMProb {
     pub l: i32,
     pub y: *const f64,
-    pub data: *const *const CSVMNode
+    pub data: *const *const CSVMNode,
 }
 
 #[repr(C)]
 pub struct CSVMNode {
     pub index: i32,
-    pub value: f64
+    pub value: f64,
 }
 
 #[allow(non_snake_case)]
@@ -30,7 +30,7 @@ pub struct CSVMParameter {
     pub nu: f64,
     pub p: f64,
     pub shrinking: i32,
-    pub probability: i32
+    pub probability: i32,
 }
 
 #[allow(non_snake_case)]
@@ -47,7 +47,7 @@ pub struct CSVMModel {
     pub sv_indices: *mut i32,
     pub label: *mut i32,
     pub nSV: *mut i32,
-    pub free_sv: i32
+    pub free_sv: i32,
 }
 
 extern "C" {
@@ -56,18 +56,28 @@ extern "C" {
     pub fn svm_load_model(model_file_name: *const i8) -> *mut CSVMModel;
     pub fn svm_train(prob: *const CSVMProb, param: *const CSVMParameter) -> *mut CSVMModel;
     pub fn svm_predict(model: *const CSVMModel, x: *const CSVMNode) -> f64;
-    pub fn svm_cross_validation(prob: *const CSVMProb, param: *const CSVMParameter, nr_fold: i32,
-                            target: *mut f64) -> ();
+    pub fn svm_cross_validation(
+        prob: *const CSVMProb,
+        param: *const CSVMParameter,
+        nr_fold: i32,
+        target: *mut f64,
+    ) -> ();
     pub fn svm_get_svm_type(model: *const CSVMModel) -> i32;
     pub fn svm_get_nr_class(model: *const CSVMModel) -> i32;
     pub fn svm_get_nr_sv(model: *const CSVMModel) -> i32;
     pub fn svm_get_labels(model: *const CSVMModel, label: *mut i32) -> ();
     pub fn svm_get_sv_indices(model: *const CSVMModel, sv_indices: *mut i32) -> ();
     pub fn svm_get_svr_probability(model: *const CSVMModel) -> f64;
-    pub fn svm_predict_values(model: *const CSVMModel, x: *const CSVMNode,
-                          dec_values: *const f64) -> f64;
-    pub fn svm_predict_probability(model: *const CSVMModel, x: *const CSVMNode,
-                               prob_estimates: *const f64) -> f64;
+    pub fn svm_predict_values(
+        model: *const CSVMModel,
+        x: *const CSVMNode,
+        dec_values: *const f64,
+    ) -> f64;
+    pub fn svm_predict_probability(
+        model: *const CSVMModel,
+        x: *const CSVMNode,
+        prob_estimates: *const f64,
+    ) -> f64;
     pub fn svm_check_parameter(prob: *const CSVMProb, param: *const CSVMParameter) -> *const i8;
     pub fn svm_free_and_destroy_model(model_ptr_ptr: *const *const CSVMModel);
 }
